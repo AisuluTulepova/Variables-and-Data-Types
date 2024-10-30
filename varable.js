d5e0219cfa50b7c52e8005c9510965f21d02eab7
Aisulu <aisulutulepova4@gmail.com>
2024-10-30 12:06:19 +0500

    comment

---
+++ varable.js
@@ -0,0 +1,34 @@
+/*let name = "Amjilt Cyber School"
+
+if(true){
+    let name = "Aisulu"
+    console.log( "Условия ишинде аты:"+ name);
+}
+console.log("Жалпы аты:" + name)*/
+
+/*const name = "Aisulu"
+name = 'Erkejan'
+console.log("menig atym:" + name)*/
+
+/*const name = " Aisulu"
+let age = "5"
+let age2 = "10"
+console.log(age + age2);*/
+let pet = "cat";
+const birthYear = 2010;
+var isStudent = true;
+
+if (true) {
+    let pet = dog;//жаңа  мәнге ие
+    console.log(pet); //dog
+}
+
+birthYear = 2000; //қате мән бере алмаймыз
+
+if (true){
+    var isStudent = false;
+}
+ 
+console.log(birthYear);
+console.log(pet);
+console.log(isStudent);
